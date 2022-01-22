c75a4a0b48a035d0ea45e393f395ed7bda9fd323
Hayden Ridgeway <haydenridgeway@gmail.com>
2022-01-22 02:31:48 -0500

    retry

--- HeathensSalvation/Source/HeathensSalvation/HeathensSalvation.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "HeathensSalvation.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HeathensSalvation, "HeathensSalvation" );
-
-DEFINE_LOG_CATEGORY(LogHeathensSalvation)
- 
