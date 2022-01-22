65b2fcf56923aa5fe8bc1bc051d0e63118b845e3
Hayden Ridgeway <haydenridgeway@gmail.com>
2022-01-22 18:27:59 -0500

    Some AI stuff before it breaks again

---
+++ HeathansSalvation/Source/HeathansSalvation/Private/gameplay/ai/OverworldEnemyController.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "gameplay/ai/OverworldEnemyController.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "UObject/ConstructorHelpers.h"
+#include "HeathansSalvation/HeathansSalvationCharacter.h"
+
+AOverworldEnemyController::AOverworldEnemyController(FObjectInitializer const& ObjectInitializer)
+{
+	static ConstructorHelpers::FObjectFinder<UBehaviorTree> obj(TEXT("BehaviorTree'/Game/TopDownCPP/Gameplay/AI/OverworldEnemy_BT.OverworldEnemy_BT'"));
+	if (obj.Succeeded())
+	{
+		BehaviorTree = obj.Object;
+	}
+	BehaviorTreeComponent = ObjectInitializer.CreateDefaultSubobject<UBehaviorTreeComponent>(this, TEXT("BehaviorComponent"));
+	Blackboard = ObjectInitializer.CreateDefaultSubobject<UBlackboardComponent>(this,TEXT("Blackboard"));
+}	
+
+void AOverworldEnemyController::BeginPlay()
+{
+	Super::BeginPlay();
+	RunBehaviorTree(BehaviorTree);
+	BehaviorTreeComponent->StartTree(*BehaviorTree);
+}
+
+void AOverworldEnemyController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	if(Blackboard)
+	{
+		Blackboard->InitializeBlackboard(*BehaviorTree->BlackboardAsset);
+	}
+}
+
+UBlackboardComponent* AOverworldEnemyController::GetBlackboard() const
+{
+	return Blackboard;
+}
+
